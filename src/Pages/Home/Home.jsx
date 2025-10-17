c86ef55f6d4b67ccc40bc5c77675ed27eafe19e3
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-10-17 16:08:12 +0600

    added the home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,16 @@
-// import { useLoaderData } from "react-router-dom";
-// import TabCatagory from "../../Components/TabCatagory/TabCatagory";
-// import SweiperSlider from "./SweiperSlider";
+import { useLoaderData } from "react-router-dom";
+import TabCatagory from "../../Components/TabCatagory/TabCatagory";
+import SweiperSlider from "./SweiperSlider";
 
-// const Home = () => {
-//   return (
-//     <div className="w-full pt-2 px-4  md:px-12 lg:px-20">
-//       <SweiperSlider></SweiperSlider>
-//       <section id="tab-category" className="mt-10">
-//         <TabCatagory/>
-//       </section>
-//     </div>
-//   );
-// };
+const Home = () => {
+  return (
+    <div className="w-full pt-2 px-4  md:px-12 lg:px-20">
+      <SweiperSlider></SweiperSlider>
+      <section id="tab-category" className="mt-10">
+        <TabCatagory/>
+      </section>
+    </div>
+  );
+};
 
-// export default Home;
+export default Home;
